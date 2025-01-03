7b46890cf469201f5594ab4af23872d4e76daf4c
janada jayathilaka <86776703+JanadaJayathilaka@users.noreply.github.com>
2025-01-03 08:57:22 +0530

    solve the issues of compatible with profile and home

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -25,15 +25,27 @@ export const Profile = () => {
         <form action="">
           <div className="form-group">
             <label>Username</label>
-            <input type="text" placeholder="Enter your username" />
+            <input
+              className="profile-input"
+              type="text"
+              placeholder="Enter your username"
+            />
           </div>
           <div className="form-group">
             <label>Email</label>
-            <input type="email" placeholder="Enter your email" />
+            <input
+              className="profile-input"
+              type="email"
+              placeholder="Enter your email"
+            />
           </div>
           <div className="form-group">
             <label>Password</label>
-            <input type="password" placeholder="Enter your password" />
+            <input
+              className="profile-input"
+              type="password"
+              placeholder="Enter your password"
+            />
           </div>
 
           <div className="form-group">
