1bb0ba861e2ed36bb6f096fdcc258a20c9d296da
Thusal Dilhara <thusaldilhara20@gmail.com>
2025-03-05 19:45:07 +0530

    added css

--- src/components/PlaylistItem.jsx
+++ src/components/PlaylistItem.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import '../styles/Home.css' 
+import { FaPlay } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 
 
-const AlbumItem = ({title,desc,img}) => {
+const PlaylistItem = ({title,desc,img,playlistId}) => {
+  const navigate = useNavigate(); 
+
+  const handlePlaylistClick = () => {
+   
+    navigate(`/playlist/${playlistId}`);
+  };
   return (
     <div className='card'>
-        <img src={img} alt={title} />
+        <img src={img} alt={title}  onClick={handlePlaylistClick} />
         <h5>{title}</h5>
         <p>{desc}</p>
+        <button className="play-button" onClick={handlePlaylistClick}>
+               <FaPlay className="play-icon" /> 
+              </button>
         
 
     </div>
   )
 }
 
-export default AlbumItem;
+export default PlaylistItem;
