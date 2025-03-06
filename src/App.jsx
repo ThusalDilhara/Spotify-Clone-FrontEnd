de40042af844a70eff18eff6dd8124952c600fdf
Thusal Dilhara <thusaldilhara20@gmail.com>
2025-03-06 10:12:41 +0530

    added song to pass as a prop to music player

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -24,9 +24,18 @@ import AboutUs from "./components/AboutUs";
 import ArtistImageUpload from "./test/uploadImage";
 import UploadSong from "./test/uploadSong";
 import PlaylistPage from "./components/PlaylistPage";
+import axios from "axios";
 
 function App() {
   const updateSongRef = useRef(null);
+  const [songs, setSongs] = useState([]); 
+
+  useEffect(() => {
+
+     axios.get('http://localhost:8080/api/songs/getdetails')
+    .then((response)=> setSongs(response.data))
+  }
+  , []);
 
   const Success = () => {
     useEffect(() => {
@@ -102,133 +111,90 @@ function App() {
           <Route path="/success" element={<Success />} />
           <Route path="/cancel" element={<Cancel />} />
 
+          {/* Home Page Route */}
           <Route
             path="/home"
             element={
               <ProtectedRoute>
                 <div>
-                  <Header
-                    updateSong={(song) =>
-                      updateSongRef.current && updateSongRef.current(song)
-                    }
-                  />
+                  <Header updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} />
                   <Sidebar />
-                  <Home
-                    updateSong={(song) =>
-                      updateSongRef.current && updateSongRef.current(song)
-                    }
+                  <Home 
+                    updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} 
+                    setSongs={setSongs} // Update song list
                   />
-                  <MusicPlayer
-                    updateSong={(fn) => (updateSongRef.current = fn)}
-                  />
+                  <MusicPlayer updateSong={(fn) => (updateSongRef.current = fn)} songs={songs} />
                 </div>
               </ProtectedRoute>
             }
           />
+
+          {/* Artist Page Route */}
           <Route
             path="/artist/:artistId"
             element={
               <div>
-                <Header
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
-                />
+                <Header updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} />
                 <Sidebar />
-                <ArtistPage
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
+                <ArtistPage 
+                  updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} 
+                  setSongs={setSongs} 
                 />
-                <MusicPlayer
-                  updateSong={(fn) => (updateSongRef.current = fn)}
-                />
+                <MusicPlayer updateSong={(fn) => (updateSongRef.current = fn)} songs={songs} />
               </div>
             }
           />
-           <Route
+
+          {/* Playlist Page Route */}
+          <Route
             path="/playlist/:playlistId"
             element={
               <div>
-                <Header
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
-                />
+                <Header updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} />
                 <Sidebar />
-                <PlaylistPage
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
+                <PlaylistPage 
+                  updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} 
+                  setSongs={setSongs} 
                 />
-                <MusicPlayer
-                  updateSong={(fn) => (updateSongRef.current = fn)}
-                />
+                <MusicPlayer updateSong={(fn) => (updateSongRef.current = fn)} songs={songs} />
               </div>
             }
           />
+
+          {/* Liked Songs Page Route */}
           <Route
             path="/likedSong"
             element={
               <div>
-                <Header
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
-                />
+                <Header updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} />
                 <Sidebar />
-                <LikedSongPage
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
+                <LikedSongPage 
+                  updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} 
+                  setSongs={setSongs} 
                 />
-                <MusicPlayer
-                  updateSong={(fn) => (updateSongRef.current = fn)}
-                />
+                <MusicPlayer updateSong={(fn) => (updateSongRef.current = fn)} songs={songs} />
               </div>
             }
           />
+
+          {/* Followed Artist Page Route */}
           <Route
             path="/followedArtist"
             element={
               <div>
-                <Header
-                  updateSong={(song) =>
-                    updateSongRef.current && updateSongRef.current(song)
-                  }
-                />
+                <Header updateSong={(song) => updateSongRef.current && updateSongRef.current(song)} />
                 <Sidebar />
-                <FollowedArtistPage />
-                <MusicPlayer
-                  updateSong={(fn) => (updateSongRef.current = fn)}
-                />
+                <FollowedArtistPage setSongs={setSongs} />
+                <MusicPlayer updateSong={(fn) => (updateSongRef.current = fn)} songs={songs} />
               </div>
             }
           />
-          <Route
-            path="/artistLogging"
-            element={
-              <div>
-                <ArtistLogging />
-              </div>
-            }
-          />
-          <Route
-            path="/artistSigning"
-            element={
-              <div>
-                <ArtistSigning />
-              </div>
-            }
-          />
-          <Route
-            path="/artistDashboard"
-            element={
-              <div>
-                <ArtistDashBoard />
-              </div>
-            }
-          />
+
+          {/* Artist Logging & Dashboard Routes */}
+          <Route path="/artistLogging" element={<ArtistLogging />} />
+          <Route path="/artistSigning" element={<ArtistSigning />} />
+          <Route path="/artistDashboard" element={<ArtistDashBoard />} />
+
         </Routes>
       </BrowserRouter>
     </div>
